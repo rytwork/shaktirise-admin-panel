a6b3d8b6978f4f65bfea66ac13ec58022308ad06
rytwork <reimaginedyieldtechnology@gmail.com>
2026-03-27 05:04:41 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["picsum.photos"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
   },
 };
 
